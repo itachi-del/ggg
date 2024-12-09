e3b4ca6fe162966e832dbd4668b9a4e29e6380e8
esim <esekenov.e05@gmail.com>
2024-12-09 10:50:17 +0500

    Changes to be committed: 	new file:   ggg.html  	new file:   ggg.js

---
+++ ggg.js
@@ -0,0 +1,87 @@
+
+
+// let course ={ 
+//          course_aty:"ggg",
+//          price: [
+//              {
+//                 name:"banan",
+//                 baga:"7000"
+                 
+//              },
+//              {
+//                 name:"alma",
+//                  baga:"7000"
+//              },
+//              {
+//                 name:"apelsin",
+//                 baga:"7000"
+//             },
+//         ]
+         
+    
+//      }
+    
+    
+
+// course.price[2].name = "ggg"
+// course.price[2].name 
+// console.log(course.price);
+
+// let flatted = course.price.flatMap(baga => baga.baga)
+// console.log(flatted);
+
+
+
+// let flatted = []
+// for(let i=0; i<course.mugalimder.length;i++){
+// flatted.push(course.baga[i].baga)
+// }
+
+// console.log(flatted.join(","));
+
+
+
+
+let shop = {
+    name: "banan", "apple",
+    Number:+77777777777,
+    price = 888888,
+    product : [
+{
+    productname: "banan",
+    categori: "ягоды",
+    price: "888888",
+    stock: "888",
+},
+
+{
+productname:"apple",
+categori:"ягоды",
+price:"888888",
+stock:"1"
+}
+    ]
+}
+
+course.name[2].name  
+course.name[2].name = "ggg"
+ console.log(shop.price);
+
+ let flatte = shop.price.flatMap(price => price.price)
+ console.log(flatted);
+
+
+
+ let flatted = []
+ for(let i=0; i<shop.shop.length;i++){
+ flatted.push(course.price[i].price)
+ }
+
+ console.log(flatted.join(","));
+
+
+
+
+
+
+
